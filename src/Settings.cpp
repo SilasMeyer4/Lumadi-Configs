568312f2d3c4096682e93e39c647d588798bc4d6
SilasMeyer4 <meyer-si4@web.de>
2026-07-08 01:03:00 +0200

    Initial commit

---
+++ src/Settings.cpp
@@ -0,0 +1,17 @@
+//
+// Created by meyer on 08.07.2026.
+//
+
+void AppSettings::Save()
+{
+}
+
+AppSettings *AppSettings::LoadFromPath(std::string &path)
+{
+  return nullptr;
+}
+
+std::string AppSettings::ToJson()
+{
+  return std::string();
+}
