31e98e852def5e00468beb5281522ef5fb9fe155
SilasMeyer4 <meyer-si4@web.de>
2026-07-08 23:36:25 +0200

    Implemented Config

---
+++ include/lumadi/config/detail/Configs.h
@@ -0,0 +1,144 @@
+//
+// Created by meyer on 08.07.2026.
+//
+
+#ifndef LUMADI_CONFIGS_SETTINGS_H
+#define LUMADI_CONFIGS_SETTINGS_H
+
+#include <string>
+#include <vector>
+#include <functional>
+#include <memory>
+#include "nlohmann/json.hpp"
+#include "SettingType.h"
+
+class ISetting
+{
+public:
+  virtual ~ISetting() = default;
+  [[nodiscard]] virtual std::string_view GetName() const = 0;
+  virtual void SetFromJson(const nlohmann::json &value) = 0;
+  [[nodiscard]] virtual nlohmann::json GetJson() const = 0;
+  virtual void SetValue(const nlohmann::json &value) = 0;
+  [[nodiscard]] virtual SettingType GetType() const = 0;
+};
+
+
+template <typename T>
+class Setting : public ISetting
+{
+public:
+  using Callback = std::function<void(const T &)>;
+
+  Setting(std::string name, T value, T defaultValue) : mName(std::move(name)), mValue(value), mDefaultValue(defaultValue)
+  {
+  }
+
+  [[nodiscard]] std::string_view GetName() const override
+  {
+    return mName;
+  }
+
+  void SetValue(const nlohmann::json &value) override
+  {
+    SetValue(value.get<T>());
+  }
+
+  [[nodiscard]] nlohmann::json GetJson() const override
+  {
+    return mValue;
+  }
+
+  void SetValue(T value) override
+  {
+    mValue = std::move(value);
+
+    if (mCallback)
+    {
+      mCallback(mValue);
+    }
+  }
+
+  const T &Get() const
+  {
+    return mValue;
+  }
+
+  const T &GetDefaultValue() const
+  {
+    return mDefaultValue;
+  }
+
+  void OnChanged(Callback callBack)
+  {
+    mCallback = std::move(callBack);
+  }
+
+  [[nodiscard]] SettingType GetType() const override
+  {
+    return SettingType::Undefined;
+  }
+
+private:
+  std::string mName;
+  T mValue;
+  T mDefaultValue;
+  Callback mCallback;
+};
+
+class IAppConfigCategory
+{
+public:
+  virtual ~IAppConfigCategory() = default;
+  virtual std::vector<std::unique_ptr<ISetting>> &GetSettings() = 0;
+  [[nodiscard]] virtual std::string_view GetName() const = 0;
+};
+
+class AppConfigCategory : public IAppConfigCategory
+{
+  public:
+  explicit AppConfigCategory(std::string name) : mName(std::move(name)){}
+  AppConfigCategory(std::string name, std::string description) : mName(std::move(name)), mDescription(std::move(description)){}
+
+
+  template<typename T>
+  Setting<T>& AddSetting(std::string name, T defaultValue)
+  {
+    auto option = std::make_unique<Setting<T>>(std::move(name), defaultValue, defaultValue);
+    auto* ptr = option.get();
+    mSettings.push_back(std::move(option));
+    return *ptr;
+  }
+
+  std::vector<std::unique_ptr<ISetting>>& GetSettings() override
+  {
+    return mSettings;
+  }
+
+  [[nodiscard]] std::string_view GetName() const override
+  {
+    return mName;
+  }
+
+  private:
+  std::string mName;
+  std::string mDescription;
+  std::vector<std::unique_ptr<ISetting>> mSettings;
+};
+
+class AppSettings
+{
+public:
+  AppSettings(std::string path, std::string versionString = "");
+  void Save();
+  [[nodiscard]] std::string GetJson() const;
+  void SetPath(std::string path);
+
+private:
+  void Load();
+  [[nodiscard]] nlohmann::json ToJson() const;
+  std::vector<std::unique_ptr<AppConfigCategory>> mSettings;
+  std::string mPath;
+  std::string mVersionString;
+};
+#endif //LUMADI_CONFIGS_SETTINGS_H
---
+++ src/Configs.cpp
@@ -0,0 +1,76 @@
+//
+// Created by meyer on 08.07.2026.
+//
+
+#include "lumadi/config/detail/Configs.h"
+
+#include <fstream>
+#include <utility>
+
+#include "nlohmann/json.hpp"
+
+AppSettings::AppSettings(std::string path, std::string versionString) : mPath(std::move(path)), mVersionString(std::move(versionString))
+{
+  Load();
+}
+
+void AppSettings::Save()
+{
+  const auto root = AppSettings::ToJson();
+
+  std::ofstream file(mPath);
+  file << root.dump(4);
+  file.close();
+}
+
+void AppSettings::Load()
+{
+  std::ifstream file(mPath);
+
+  nlohmann::json root;
+  file >> root;
+
+   mVersionString = root.value("version", "");
+
+  for (auto& category : mSettings)
+  {
+    if (!root.contains(category->GetName()))
+      continue;
+
+    auto& cat = root[category->GetName()];
+
+    for (auto& option : category->GetSettings())
+    {
+      if (!cat.contains(option->GetName()))
+        continue;
+
+      option->SetFromJson(cat[option->GetName()]);
+    }
+  }
+  file.close();
+}
+
+std::string AppSettings::GetJson() const
+{
+  return ToJson().dump(4);
+}
+
+void AppSettings::SetPath(std::string path)
+{
+  mPath = std::move(path);
+}
+
+nlohmann::json AppSettings::ToJson() const
+{
+  nlohmann::json root;
+  root["version"] = mVersionString;
+  for (auto& category : mSettings)
+  {
+    for (auto& option : category->GetSettings())
+    {
+      root[category->GetName()][option->GetName()] = option->GetJson();
+    }
+  }
+
+  return root;
+}
